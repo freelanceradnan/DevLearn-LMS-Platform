6466c6faf3f3681a98e803e1ebcb66e7d2300b0c
freelanceradnan <reactorbro722@gmail.com>
2026-08-06 01:16:57 +0600

    api slice added

---
+++ client/src/Features/ApiSlice.js
@@ -0,0 +1,58 @@
+import { fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { Mutex } from "async-mutex";
+
+const mutex = new Mutex();
+
+const BaseQuery = fetchBaseQuery({
+  baseUrl: import.meta.env.VITE_BASE_SERVER,
+  credentials: "include", 
+});
+
+export const baseQueryWithReauth = async (args, api, extraOptions) => {
+
+  await mutex.waitForUnlock();
+
+  let result = await BaseQuery(args, api, extraOptions);
+
+  if (result?.error && result.error.status === 401) {
+    if (!mutex.isLocked()) {
+      const release = await mutex.acquire();
+
+      try {
+   
+        const refreshResult = await BaseQuery(
+          {
+            url: "/updateAccesstoken",
+            method: "GET",
+          },
+          api,
+          extraOptions
+        );
+
+        if (refreshResult?.data) {
+     
+          result = await BaseQuery(args, api, extraOptions);
+        } else {
+          handleLogout();
+        }
+      } finally {
+        release();
+      }
+    } else {
+  
+      await mutex.waitForUnlock();
+      result = await BaseQuery(args, api, extraOptions);
+    }
+  }
+
+  return result;
+};
+
+const handleLogout = () => {
+  if (
+    !window.location.pathname.includes("/login") &&
+    !window.location.pathname.includes("/register")
+  ) {
+    window.location.href = "/login"; 
+  }
+};
